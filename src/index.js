8322761b75007b9d45fe952d426eef097d7aa89e
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2024-02-20 15:33:46 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import _ from 'lodash';
+import './assets/style.css';
+import './assets/normalize.css';
+
+//Undefined variables for weather data that will be given a value once the weather data is fetched
+let location, temp, feelsTemp, weatherCondition, humidity, precip, wind, windDirection, country, city, localTime;
+
+async function getWeatherData(location) {
+    try {
+        const response = await fetch (`https://api.weatherapi.com/v1/current.json?key=fb4d08953c954f34a8031133240602&q=${location}`, {mode: "cors"}); //Gets the weather data from Weather API
+
+        if (!response.ok) {
+            throw new Error (`There is a problem getting the response.`);
+        }
+
+        const weatherData = await response.json(); //Extracts the data using json method
+
+        //Set the data to undefined variables
+        const { //Destructuring assignment allows to access the properties and assign them to their respective variables
+            temp_c: temp,
+            feelslike_c: feelsTemp,
+            condition: {text: weatherCondition},
+            humidity: humidity,
+            precip_mm: precip,
+            wind_kph: wind,
+            wind_dir: windDirection,
+        } = weatherData.current
+
+        const { //Destructuring assignment allows to access the properties and assign them to their respective variables
+            country: country,
+            name: city,
+            localtime: localTime,
+        } = weatherData.location
+
+        const weatherInfo = {
+            temp: `Temperature: ${temp}°C`,
+            feelsTemp: `Feels like: ${feelsTemp}°C`,
+            weatherCondition: `Condition: ${weatherCondition}`,
+            humidity: `Humidity: ${humidity}`,
+            precip: `Precipitation: ${precip}mm`,
+            wind: `Wind speed: ${wind}kph`,
+            windDirection: `Wind direction: ${windDirection}`,
+            country: `Country: ${country}`,
+            city: `City: ${city}`,
+            localTime: `Local time: ${localTime}`,
+        };
+
+        if (location == undefined || location == null) {
+            throw new Error ("Wrong or invalid location");
+        }
+        
+        console.log(location);
+        console.log(weatherData);
+        return { weatherInfo };
+    } 
+    
+    catch (error) {
+        console.error("Wrong or invalid location!");
+    }
+}
+
+//Event listener for searching city or country
+const searchForCity = document.getElementById("search-form");
+searchForCity.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const location = document.getElementById("search").value;
+    getWeatherData(location);
+});
