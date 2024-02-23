c419e40fe8031c02886f558ee1fda8e344f450d5
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2024-02-23 16:44:27 +0800

    Add container for date and time

--- src/DOM.js
+++ src/DOM.js
@@ -1,8 +1,9 @@
+import { format, formatDate } from "date-fns";
+
 export function displayInfo(
     temp, 
     feelsTemp, 
     weatherCondition,
-    conditionIcon, 
     humidity, 
     precip, 
     wind, 
@@ -13,50 +14,29 @@ export function displayInfo(
     willItRain) {
         const mainInfo = document.querySelector(".weather-info");
         mainInfo.textContent = "";
-
-        //Create h3 element for weather condition
+        //Displays weather condition
         const condition = document.createElement("h2");
-        //Create img element for icon next to weather condition text
-        const conditionImg = document.createElement("img");
-        //Set value of weatherCondition to h2 element
         condition.append(`${weatherCondition}`);
-        //Append icon to img element
-        conditionImg.src = conditionIcon;
-        //Append h3 element to mainInfo
-        //Append img element to mainInfo
         mainInfo.append(condition);
-        mainInfo.append(conditionImg);
-
-        //Create h3 element for city, country
-        const place = document.createElement("h3");
-        //Set value of city and country to h3
+        //Displays place (City, country format)
+        const place = document.createElement("h2");
         place.append(`${city}, ${country}`);
-        //Append the element to mainInfo
         mainInfo.append(place);
-        
-        //Create h1 element for temperature
+        //Displays temperature and other info
         const temperature = document.createElement("h1");
-        //Add class to h1 for customization
         temperature.classList.add("temperature");
-        //Set value of temp to h1 element
-        temperature.append(temp);
-        //Add p elements for: feels like, humidity, and chance of rain
-        const feelsLikeTemp = document.createElement("p");
-        const humid = document.createElement("p");
-        const chanceofRain = document.createElement("p");
-        //Set value of feelsLike, humidity, and willItRain to respective elements
-        feelsLikeTemp.append(feelsTemp);
-        humid.append(humidity);
-        chanceofRain.append(willItRain);
-        //Append h1 element to mainInfo
+        temperature.append(`${temp}°C`);
         mainInfo.append(temperature);
-        mainInfo.append(feelsLikeTemp);
-        mainInfo.append(humid);
-        mainInfo.append(chanceofRain);
-
-        //Create h3 element for date and time
-        const dateAndTime = document.createElement("h3");
-        //Set value of date and time to the element
-        dateAndTime.append(localTime);
+        //Displays date and time
+        const date = document.createElement("h3");
+        const time = document.createElement("h3");
+        time.classList.add("time");
+        const dateAndTime = document.createElement("div");
+        const formatDate = format(new Date(localTime), "MMMM dd, yyyy");
+        const formatTime = format(new Date(localTime), "H:mm");
+        date.append(formatDate);
+        time.append(formatTime);
+        dateAndTime.append(date);
+        dateAndTime.append(time);
         mainInfo.append(dateAndTime);
 }
