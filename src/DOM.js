6ca156db2eb3ffa9a95a004d656367a83134404b
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2024-02-25 16:09:55 +0800

    Create function that displays hourly information

--- src/DOM.js
+++ src/DOM.js
@@ -6,37 +6,56 @@ export function displayInfo(
     weatherCondition,
     humidity, 
     precip, 
-    wind, 
-    windDirection, 
     country, 
     city, 
     localTime,
-    willItRain) {
+    willItRain
+    ) {
         const mainInfo = document.querySelector(".weather-info");
-        mainInfo.textContent = "";
+        mainInfo.innerHTML = "";
+
         //Displays weather condition
         const condition = document.createElement("h2");
+        condition.classList.add("condition");
         condition.append(`${weatherCondition}`);
         mainInfo.append(condition);
+
         //Displays place (City, country format)
         const place = document.createElement("h2");
+        place.classList.add("place");
         place.append(`${city}, ${country}`);
         mainInfo.append(place);
+
         //Displays temperature and other info
         const temperature = document.createElement("h1");
         temperature.classList.add("temperature");
         temperature.append(`${temp}°C`);
         mainInfo.append(temperature);
+
         //Displays date and time
-        const date = document.createElement("h3");
-        const time = document.createElement("h3");
-        time.classList.add("time");
-        const dateAndTime = document.createElement("div");
+        const dateAndTime = document.createElement("h3");
         const formatDate = format(new Date(localTime), "MMMM dd, yyyy");
         const formatTime = format(new Date(localTime), "H:mm");
-        date.append(formatDate);
-        time.append(formatTime);
-        dateAndTime.append(date);
-        dateAndTime.append(time);
+        dateAndTime.append(`${formatDate} | ${formatTime}`);
         mainInfo.append(dateAndTime);
+}
+
+export function displayHourly(
+    hourlyTemp,
+    weatherIcon
+) {
+    const hourlyForecast = document.querySelector(".hourly-forecast");
+    const hourlyContainer = document.createElement("div");
+
+    //Displays weather icon
+    const icon = document.createElement("img");
+    icon.src = weatherIcon;
+    hourlyContainer.append(icon);
+
+    //Displays hourly temp
+    const hourlyTemperature = document.createElement("p");
+    hourlyTemperature.append(`${hourlyTemp}°C`);
+    hourlyContainer.append(hourlyTemperature);  
+    
+    hourlyForecast.append(hourlyContainer);
 }
