d5ae3d8c3e09eac8432d91cf489023e63b86add3
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2024-02-27 15:04:37 +0800

    Install file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'src/assets/[name].[ext]'
   },
   devtool: 'inline-source-map',
   devServer: {
