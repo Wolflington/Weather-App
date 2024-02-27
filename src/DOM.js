48768f243958da53076dce421a87a6acb432b186
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2024-02-27 15:01:39 +0800

    Append feels temp, humidity, and chance of rain

--- src/DOM.js
+++ src/DOM.js
@@ -1,4 +1,11 @@
-import { format, formatDate } from "date-fns";
+import { format } from "date-fns";
+import searchImg from './assets/search-icon.png';
+import feelsLikeIcon from './assets/thermometer.png';
+import rainIcon from './assets/rain.png';
+import humidityIcon from './assets/humidity.png';
+
+const searchIcon = document.getElementById("search-icon");
+searchIcon.setAttribute("src", searchImg);
 
 export function displayInfo(
     temp, 
@@ -16,19 +23,16 @@ export function displayInfo(
 
         //Displays weather condition
         const condition = document.createElement("h2");
-        condition.classList.add("condition");
         condition.append(`${weatherCondition}`);
         mainInfo.append(condition);
 
         //Displays place (City, country format)
         const place = document.createElement("h2");
-        place.classList.add("place");
         place.append(`${city}, ${country}`);
         mainInfo.append(place);
 
         //Displays temperature and other info
         const temperature = document.createElement("h1");
-        temperature.classList.add("temperature");
         temperature.append(`${temp}°C`);
         mainInfo.append(temperature);
 
@@ -38,6 +42,51 @@ export function displayInfo(
         const formatTime = format(new Date(localTime), "H:mm");
         dateAndTime.append(`${formatDate} | ${formatTime}`);
         mainInfo.append(dateAndTime);
+
+        const additionalInfo = document.querySelector(".additional-info");
+
+        //Displays feels like temperature
+        const feelsLikeContainer = document.createElement("div");
+        feelsLikeContainer.classList.add("feels-like");
+        const feelsLikeDetails = document.createElement("div");
+        const feelsLikeText = document.createElement("p");
+        feelsLikeText.textContent = "Feels like";
+        const feelsLikeTemp = document.createElement("h2");
+        feelsLikeTemp.textContent = `${feelsTemp}°C`;
+        const tempIcon = document.createElement("img");
+        tempIcon.src = feelsLikeIcon;
+
+        feelsLikeDetails.append(feelsLikeText, feelsLikeTemp);
+        feelsLikeContainer.append(tempIcon, feelsLikeDetails);
+        additionalInfo.append(feelsLikeContainer);
+
+        //Displays humidity
+        const humidityContainer = document.createElement("div");
+        const humidityDetails = document.createElement("div");
+        const humidityText = document.createElement("p");
+        humidityText.textContent = "Humidity"
+        const humidityPercent = document.createElement("h2");
+        humidityPercent.textContent = `${humidity}%`;
+        const humidityPic = document.createElement("img");
+        humidityPic.src = humidityIcon;
+
+        humidityDetails.append(humidityText, humidityPercent);
+        humidityContainer.append(humidityPic, humidityDetails);
+        additionalInfo.append(humidityContainer);
+
+        //Displays chance of raining
+        const rainContainer = document.createElement("div");
+        const rainDetails = document.createElement("div");
+        const rainText = document.createElement("p");
+        rainText.textContent = "Chance of raining";
+        const chanceOfRain = document.createElement("h2");
+        chanceOfRain.textContent = `${willItRain}%`;
+        const rainPic = document.createElement("img");
+        rainPic.src = rainIcon;
+
+        rainDetails.append(rainText, chanceOfRain);
+        rainContainer.append(rainPic, rainDetails);
+        additionalInfo.append(rainContainer);
 }
 
 export function displayHourly(
@@ -68,37 +117,34 @@ export function displayHourly(
     hourlyForecast.append(hourlyContainer);
 }
 
-//Initialize a variable that gets the element of wrapper
-const hourlyWrapper = document.querySelector(".hourly-forecast");
-//Access the dots
-let dots = document.querySelectorAll(".dot");
-//Set a variable to zero to initialize array index (which is 0)
-let activeDot = 0;
+(function changeSlide() {
+    const hourlyWrapper = document.querySelector(".hourly-forecast");
+    const prevBtn = document.querySelector(".prev");
+    const nextBtn = document.querySelector(".next");
+    let dots = document.querySelectorAll(".dot");
+    let activeDot = 0;
 
-//FOR EACH dots, pass two parameters: dot and index
-dots.forEach((dot, index) => {
-    //SET ATTRIBUTE to each dot and pass index parameter to the dots
-    dot.setAttribute("data-index", index);
-    //Add click event listener to dot, and pass "event" parameter
-    dot.addEventListener("click", (event) => {
-        //Create variable clickedDot that pass the event parameter to targeted dataset attribute
-        let clickedDot = event.currentTarget.dataset.index;
-        //IF the clicked dot is the same as active dot, do nothing
-        if (clickedDot === activeDot) {
-            return;
-        }
-        //ELSE change the clickedDot to activeDot
-        else {
-            //Initialize variable additionalInfo to get the element of wrapper and its width
-            let displayArea = hourlyWrapper.parentElement.clientWidth;
-            //Initialize variable pixels to calculate the area of pixels
-            let pixels = -displayArea * clickedDot;
-            //Apply style to wrapper to transform the wrapper
-            hourlyWrapper.style.transform = 'translateX('+ pixels + 'px)';
-            //Set the active dot to clicked dot variable
-            dots[activeDot].classList.remove("active");
-            dots[clickedDot].classList.add("active");
-            activeDot = clickedDot;
-        }
-    });
-});
+    for (let index = 0; index < dots.length; index++) {
+            let dot = dots[index];
+            dot.setAttribute("data-index", index);
+            dot.addEventListener("click", (event) => {
+            //Get the index of the dot
+            let clickedDot = event.currentTarget.dataset.index;
+            if (clickedDot === activeDot) {
+                return;
+            }
+            else {
+                //Get the parent element and its width
+                let displayArea = hourlyWrapper.parentElement.clientWidth;
+                //Calculate the area of pixels
+                let pixels = -displayArea * clickedDot;
+                //Apply style to wrapper to transform the wrapper
+                hourlyWrapper.style.transform = 'translateX('+ pixels + 'px)';
+                //Toggles active class
+                dots[activeDot].classList.remove("active");
+                dots[clickedDot].classList.add("active");
+                activeDot = clickedDot;
+            }
+        });
+    }
+})();
